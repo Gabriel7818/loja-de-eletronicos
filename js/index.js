11731df882cb6f12ffa72426835188ae921109bb
Gabriel Oliveira <Gabriel.Damasceno7818@outlook.com.br>
2024-02-17 16:56:05 -0300

    Adicionado mais produtos e também carrosel de destaques

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Inicialize o carrossel de destaques
+const destaquesSlider = tns({
+    container: '.destaques-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controls: false,
+    nav: false
+});
