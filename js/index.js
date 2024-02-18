25b3203735b6850a898820c4026981d8392c1081
Gabriel Oliveira <Gabriel.Damasceno7818@outlook.com.br>
2024-02-17 21:15:04 -0300

    Adição de novos produtos e imagens na página inicial

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-// Inicialize o carrossel de destaques
-const destaquesSlider = tns({
-    container: '.destaques-slider',
-    items: 1,
-    slideBy: 'page',
-    autoplay: true,
-    autoplayButtonOutput: false,
-    controls: false,
-    nav: false
+document.addEventListener('DOMContentLoaded', function () {
+    // Inicialize o carrossel de destaques
+    const destaquesSlider = tns({
+        container: '.destaques-slider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        autoplayButtonOutput: false,
+        controls: false,
+        nav: false
+    });
 });
